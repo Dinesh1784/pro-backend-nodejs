7c86d0c44a3d8bf50156b14677a306c700396c4b
Dinesh <d4dinesh52@gmail.com>
2022-01-11 22:43:56 +0530

    day 1

--- lesson1/index.js
+++ lesson1/index.js
@@ -4,7 +4,19 @@ const PORT = 4000 || process.env.PORT;
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Welcome to the API",
+    message: "Welcome to the Social App Api",
+  });
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+  const instaSocial = {
+    username: "ns_kid_200",
+    followers: 66,
+    follows: 1200,
+    date: Date.now(),
+  };
+  res.status(200).json({
+    instaSocial,
   });
 });
 
